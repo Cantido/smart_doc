7260ee6bb28047185c5691fb102994001ba7ced6
Rosa Richter <cosmic.lady.rosa@gmail.com>
2020-11-04 21:52:59 -0700

    New mix project

---
+++ lib/stex.ex
@@ -0,0 +1,18 @@
+defmodule Stex do
+  @moduledoc """
+  Documentation for `Stex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Stex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
